da35dfb6f124bdbbd06cdf301672f143e214af98
MrCyci6 <cyriaclen@gmail.com>
2023-10-23 00:49:40 +0200

    animation

---
+++ js/animation.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded",  () => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const bulles = [];
+
+    function creerBulle() {
+        const bulle = {
+            x: Math.random() * canvas.width,
+            y: canvas.height,
+            rayon: Math.random() * 1.5,
+            vitesseY: Math.random() * 1 + 0.5, 
+            couleur: "rgba(52, 152, 219, 0.7)"
+        };
+        bulles.push(bulle);
+    }
+
+
+    function dessinerBulles() {
+        for (let i = 0; i < bulles.length; i++) {
+            const bulle = bulles[i];
+            ctx.beginPath();
+            ctx.arc(bulle.x, bulle.y, bulle.rayon, 0, Math.PI * 2);
+            ctx.fillStyle = bulle.couleur;
+            ctx.fill();
+
+            bulle.y -= bulle.vitesseY;
+
+            if (bulle.y < -bulle.rayon) {
+                bulles.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    function animer() {
+        creerBulle();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        dessinerBulles();
+        requestAnimationFrame(animer);
+    }
+
+    animer();
+})
--- js/index.js
+++ js/index.js
@@ -18,5 +18,20 @@ firebase.auth().onAuthStateChanged(async user => {
         <a href="">Contact</a>
         <a class="btn-login" href="./profile.html"><i class="fa fa-user-circle"></i></a>
         <button id="logout" type="submit"><i class="fa fa-window-close-o"></i></button>`
+    
+        // LOGOUT
+        let logoutButton = document.getElementById("logout");
+        
+        logoutButton.addEventListener("click", e => {
+            
+            firebase.auth().signOut()
+            .then(() => {
+    
+                console.log("Déconnexion réussie.");
+                window.location.href = "./login.html"
+            }).catch(e => {
+                console.error("Erreur lors de la déconnexion : ", e);
+            });
+        });
     }
 });
